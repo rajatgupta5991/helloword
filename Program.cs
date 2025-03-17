418fa68fe281e4e08a93244f3cb95dce46b228f1
Rajat Gupta <58070119+rajatgupta5991@users.noreply.github.com>
2025-03-17 06:53:24 +0530

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Output "Hello, World!" to the console
+        Console.WriteLine("Hello, World!");
+    }
+}
